b107d98818a3d48872c56562ecd891f9d0335e35
Your Name <you@example.com>
2014-10-04 17:07:00 +0200

    providing html chunks

--- app/controllers/dynamic_pets_controller.rb
+++ app/controllers/dynamic_pets_controller.rb
@@ -1,13 +1,19 @@
 class DynamicPetsController < ApplicationController
+  layout nil
+  layout 'application', except: 'view'
+  
   def main
   end
 
   def fish
+    render partial: 'favorite_pets/fish_details'
   end
 
-  def cat
+  def cats
+    render partial: 'favorite_pets/cat_details'
   end
 
-  def dog
+  def dogs
+    render partial: 'favorite_pets/dog_details'
   end
 end
